4eacc79ce15ed5e0fcc0cd590ea1408f6a9f5907
francesco-pecora <francescogp9898@hotmail.com>
2020-04-18 00:40:51 -0400

    routes working

--- website/main.py
+++ website/main.py
@@ -1,29 +1,37 @@
-from flask import Flask, render_template, url_for, redirect, session
+from flask import Flask, render_template, url_for, redirect, session, request
+from client import Client
 
 NAME_KEY = "name"
 
 app = Flask(__name__)
 app.secret_key = "hello"
 
-'''
-@app.route("/login")
+
+@app.route("/login", methods = ["POST", "GET"])
 def login():
-    return render_template("login.html")
-'''
-'''
+    if request.method == "POST":
+        session[NAME_KEY] = request.form["inputName"]
+        return redirect(url_for("home"))
+    return render_template("login.html") #, **{"session": session}
+
+
 @app.route("/logout")
 def logout():
     session.pop(NAME_KEY, None)
     return redirect(url_for("login"))
-'''
+
 @app.route("/")
 @app.route("/home")
 def home():
-    #if NAME_KEY not in session:
-    #    return redirect(url_for("login"))
+    if NAME_KEY not in session:
+        return redirect(url_for("login"))
 
-    #name = session[NAME_KEY]
-    return render_template("index.html")
+    return render_template("index.html") #, **{"login": True, "session": session}
+
+@app.route("/run")
+def run():
+    print("clicked")
+    return "none"
 
 if __name__ == "__main__":
     app.run(debug=True)
