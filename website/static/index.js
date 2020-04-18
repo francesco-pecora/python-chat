4543f46a3d9fc9409ff8fdd06f0fa07398c5ff54
francesco-pecora <francescogp9898@hotmail.com>
2020-04-18 00:38:41 -0400

    jQuery for data communication

---
+++ website/static/index.js
@@ -0,0 +1,16 @@
+$(function(){
+    $('#test').bind('click', function(){
+        $.getJSON('/run',
+            function(data){
+                // do nothing
+            });
+        return false;
+    });
+});
+
+function validate(name){
+    if(name.length >= 2){
+        return true;
+    }
+    return false;
+}
