7d9805c53453ba07c57784003feeb372995b7d9e
francesco-pecora <francescogp9898@hotmail.com>
2020-04-19 02:23:20 -0400

    handled msg requests to server

--- website/static/index.js
+++ website/static/index.js
@@ -1,15 +1,34 @@
 $(function(){
     $('#sendBtn').bind('click', function(){
-        var value = document.getElementById("msg").value;
-        console.log(value);
-        $.getJSON('/run',
+        var msg = document.getElementById("msg");
+        var value = msg.value;
+        msg.value = ""
+        $.getJSON('/send_message',
             {val:value},
             function(data){
-
+                
             });
-        return false;
     });
 });
+
+window.onload = function(){
+    var update_loop = setInterval(update, 100);
+    update();
+}
+
+
+function update(){
+    fetch("/get_messages")
+        .then((response) => {
+            return response.json();
+        }).then((text) => {
+            var messages = "";
+            for(value of text["messages"]){
+                messages = messages + "<br >" + value; 
+            }
+            document.getElementById("test").innerHTML = messages;
+        });
+}
 
 function validate(name){
     if(name.length >= 2){
@@ -17,3 +36,4 @@ function validate(name){
     }
     return false;
 }
+
