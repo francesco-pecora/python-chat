35f5fb4dad53ddde5fc3dadf615a0200356cd204
francesco-pecora <francescogp9898@hotmail.com>
2020-04-20 22:36:36 -0400

    fixed wrong socket emit

--- website/main.py
+++ website/main.py
@@ -10,13 +10,13 @@ app = create_app()
 socketio = SocketIO(app)
 
 @socketio.on("event")
-def handle_custom_event(json, methods=["GET", "POST"]):
+def handle_my_custom_event(json, methods=["GET", "POST"]):
     data = dict(json)
     if "name" in data:
         db = DataBase()
         db.save_message(data["name"], data["message"])
     
-    socketio.emit("[MESSAGE RESPONSE]", json)
+    socketio.emit("message response", json)
 
 
 if __name__ == "__main__":
